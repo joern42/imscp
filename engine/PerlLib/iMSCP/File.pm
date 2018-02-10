e9610120dbdfb7baa8cafb2c9844ff0c569b5736
Laurent Declercq <l.declercq@nuxwin.com>
2018-02-10 02:37:43 +0100

    Fixed: Apply specific treatment on moved symlinks as their target path can be relative to their current location (iMSCP::File) CS fixes

--- engine/PerlLib/iMSCP/File.pm
+++ engine/PerlLib/iMSCP/File.pm
@@ -26,16 +26,17 @@ package iMSCP::File;
 use strict;
 use warnings;
 use Carp qw/ croak /;
+use Cwd ();
 use English;
 use Errno qw/ EPERM EINVAL ENOENT /;
 use Fcntl qw/ :mode /;
-use File::Basename;
+use File::Basename ();
 use File::Copy ();
-use File::Spec;
+use File::Spec ();
 use iMSCP::Boolean;
 use iMSCP::Umask;
 use Lchown;
-use overload '""' => "STRINGIFY", fallback => 1;
+use overload '""' => \&__toString, fallback => 1;
 use parent 'iMSCP::Common::Object';
 
 =head1 DESCRIPTION
@@ -130,6 +131,9 @@ sub save
 =item remove( )
 
  Remove this file
+ 
+ At this time, only symlinks and regular files are considered. Other files are
+ silently ignored.
 
  Return self, die on failure
 
@@ -139,9 +143,18 @@ sub remove
 {
     my ($self) = @_;
 
-    return $self unless -l $self->{'filename'} || -f _;
+    my (@st) = lstat $self->{'filename'};
 
-    unlink $self->{'filename'} or die( sprintf( "Failed to delete '%s': %s", $self->{'filename'}, $! ));
+    @st || $! == ENOENT or die( sprintf( "Failed to access '%s': %s", $self->{'filename'}, $! ));
+
+    return $self unless @st;
+
+    !S_ISDIR( $st[2] ) or croak( sprintf( "Failed to remove '%s': Not a file", $self->{'filename'} ));
+
+    # Files other than symlinks and regular files are ignored silently.
+    return $self unless S_ISLNK( $st[2] ) || S_ISREG( $st[2] );
+
+    unlink $self->{'filename'} or die( sprintf( "Failed to remove '%s': %s", $self->{'filename'}, $! ));
     $self;
 }
 
@@ -187,19 +200,19 @@ sub owner
     $self;
 }
 
-=item copy( $target [, \%options = { preserve => FALSE, no-dereference => TRUE, no-target-directory = FALSE } ] )
+=item copy( $dest [, \%options = { preserve => FALSE, no-dereference => TRUE, no-target-directory = FALSE } ] )
 
- Copy this file to the given target
+ Copy this file to the given destination
 
- - At this time, only regular and symlink files are copied. Other files are silently ignored.
- - Access Control List (ACL) are never preserved.
+ At this time, only regular and symlink files are considered. Other files are
+ silently ignored. Access Control List (ACL) are not preserved (to be fixed using Linux::ACL module).
 
  The behavior is nearly the same as the cp(1) command:
   - symlinks are never dereferenced (same as the cp(1) --no-dereference option)
   - If the 'preserve' option is TRUE (same as the cp(1) --preserve=ownership,mode option), source file's permission bits and ownership are preserved.
     Set-UID and Set-GID bit are preserved only if the copy of both owner and group succeeded.
   - If the 'preserve' option is FALSE, ownership and permission of existent files are left untouched. The following rules apply for new files:
-     - Permission will be same as original file (excluding setuid/setgid bits) with current UMASK(2) applied on them.
+     - Permission will be same as original file (excluding setuid/setgid bits) with caller or current UMASK(2) applied on them.
      - Owner is set to EUID while the group set depends on a range of factors:
        - If the fs is mounted with -o grpid, the group is made the same as that of the parent dir.
        - If the fs is mounted with -o nogrpid and the setgid bit is disabled on the parent dir, the group will be set to EGID
@@ -212,67 +225,68 @@ sub owner
 
     See http://man7.org/linux/man-pages/man1/cp.1.html for further details.
 
- Param string $target Depending on passed-in options, can be either a file path or directory path
+ Param string $dest Depending on passed-in options, can be either a file path or directory path
  Param hash \%options OPTIONAL options:
   - umask               : OPTIONAL UMASK(2) for a new file. For instance if the given umask is 0027, mode will be: SRC_MODE & ~0027 .
                           This option is only relevant when the preserve option is not TRUE.
   - preserve            : If set to TRUE preserve ownership and permissions (default FALSE).
-  - no-target-directory : If set to TRUE, treat $target as a normal file    (default FALSE).
+  - no-target-directory : If set to TRUE, treat $dest as a normal file    (default FALSE).
  Return self, die on failure
 
 =cut
 
 sub copy
 {
-    my ($self, $target, $options) = @_;
+    my ($self, $dest, $options) = @_;
     $options //= {};
 
     ref $options eq 'HASH' or croak( '$options parameter is not valid. Hash expected' );
-    defined $target or croak( '$target parameter is missing.' );
+    defined $dest or croak( '$dest parameter is missing.' );
 
     my (@sst) = lstat $self->{'filename'} or croak( sprintf( "Failed to stat '%s': %s", $self->{'filename'}, $! ));
-    my (@tst) = lstat $target;
-    @tst || $! == ENOENT or die( sprintf( "Failed to access '%s': %s", $target, $! ));
+    my (@dst) = lstat $dest;
+    @dst || $! == ENOENT or die( sprintf( "Failed to access '%s': %s", $dest, $! ));
 
-    !S_ISDIR( $sst[2] ) or croak( sprintf( "Failed to copy '%s' to '%s': '%s' is a directory", $self->{'filename'}, $target, $self->{'filename'} ));
+    !S_ISDIR( $sst[2] ) or croak( sprintf( "Failed to copy '%s' to '%s': '%s' is a directory", $self->{'filename'}, $dest, $self->{'filename'} ));
 
-    # At this moment, only regular files and symlinks are copied. Other are ignored silently.
+    # At this moment, only regular files and symlinks are copied.
+    # Other are ignored silently.
     return $self unless S_ISLNK( $sst[2] ) || S_ISREG( $sst[2] );
 
-    if ( @tst ) {
-        # Make sure that we don't try copy on ourself
-        if ( $sst[1] == $tst[1] && $sst[2] == $tst[2] ) {
-            croak( sprintf( "Failed to copy. '%s' and '%s' are the same file", $self->{'filename'}, $target ));
+    if ( @dst ) {
+        # Make sure that we don't try copy on ourself.
+        if ( $sst[1] == $dst[1] && $sst[2] == $dst[2] ) {
+            croak( sprintf( "Failed to copy. '%s' and '%s' are the same file", $self->{'filename'}, $dest ));
         }
 
-        if ( $options->{'no-target-directory'} && S_ISDIR( $tst[2] ) ) {
-            die( sprintf( "Failed to copy '%s' to '%s': cannot overwrite directory with a non-directory", $self->{'filename'}, $target ));
-        } elsif ( S_ISDIR( $tst[2] ) ) {
-            # Append the last component of $self->{'filename'} to $target.
-            $target = File::Spec->catfile( $target, basename( $self->{'filename'} ));
+        if ( $options->{'no-target-directory'} && S_ISDIR( $dst[2] ) ) {
+            die( sprintf( "Failed to copy '%s' to '%s': cannot overwrite directory with a non-directory", $self->{'filename'}, $dest ));
+        } elsif ( S_ISDIR( $dst[2] ) ) {
+            # Append the last component of $self->{'filename'} to $dest.
+            $dest = File::Spec->catfile( $dest, File::Basename::basename( $self->{'filename'} ));
         }
     }
 
     if ( S_ISLNK( $sst[2] ) ) {
         # symlink() call fails if the file already exists.
-        unlink $target;
-        symlink( readlink( $self->{'filename'} ), $target ) or die( sprintf( "Failed to copy '%s' to '%s': %s", $self->{'filename'}, $target, $! ));
+        unlink $dest;
+        symlink( readlink( $self->{'filename'} ), $dest ) or die( sprintf( "Failed to copy '%s' to '%s': %s", $self->{'filename'}, $dest, $! ));
     } else {
-        File::Copy::copy( $self->{'filename'}, $target ) or die( sprintf( "Failed to copy '%s' to '%s': %s", $self->{'filename'}, $target, $! ));
+        File::Copy::copy( $self->{'filename'}, $dest ) or die( sprintf( "Failed to copy '%s' to '%s': %s", $self->{'filename'}, $dest, $! ));
     }
 
     # Preserve ownership. CHOWN(2) turns off set[ug]id bits for non-root, so do
     # the CHMOD(2) last.
     my $ownershipFailed = FALSE;
-    if ( $options->{'preserve'} && !lchown( $sst[4], $sst[5], $target ) ) {
+    if ( $options->{'preserve'} && !lchown( $sst[4], $sst[5], $dest ) ) {
         # If a non-root user pass the preserve option when copying file, it's ok
         # if we can't preserve ownership. But root probably wants to know, e.g.
         # if NFS disallows it, or if the target system doesn't support file ownership.
-        ( $! == EPERM || $! == EINVAL ) && $EUID != 0 or die sprintf( "Failed to preserve ownership for '%s': %s", $target, $! );
+        ( $! == EPERM || $! == EINVAL ) && $EUID != 0 or die sprintf( "Failed to preserve ownership for '%s': %s", $dest, $! );
 
         # Failing to preserve ownership is OK. Still, try to preserve
         # the group, but ignore the possible error.
-        lchown( -1, $sst[5], $target );
+        lchown( -1, $sst[5], $dest );
 
         $ownershipFailed = TRUE;
     }
@@ -280,56 +294,88 @@ sub copy
     # We do not want call CHMOD(2) on symlinks
     return $self if S_ISLNK( $sst[2] );
 
-    # We do not want preserve permissions and $target was an existent file
-    # In such a case, file must be left untouched
-    return $self if @tst && !$options->{'preserve'};
+    # We do not want preserve permissions and $dest was an existent file.
+    # In such a case, file must be left untouched.
+    return $self if !$options->{'preserve'} && @dst;
 
     my $mode;
     if ( $options->{'preserve'} ) {
         # we do want preserve permissions
         $mode = $sst[2];
-        # If preserving owner and group was not possible the setuid and/or setgid bits must be cleared
+        # If preserving owner and group was not possible the setuid and/or
+        # setgid bits must be cleared.
         $mode &= ~06000 if $ownershipFailed;
     } else {
-        # Change current umask if needed
-        local $UMASK = $self->{'umask'} if defined $options->{'umask'};
-
-        # We do not want preserve permissions and file was not existent
-        # In such case, we set original permissions (excluding setuid/setgid bits), applying current umask on them
-        $mode = ( $sst[2] & ~( umask || 0 ) ) & ~06000;
+        # We do not want preserve permissions and $dest was not an existent file.
+        # In such case, we set original permissions (excluding setuid/setgid bits),
+        # applying caller or current umask on them.
+        $mode = ( $sst[2] & ~( $options->{'umask'} // $UMASK ) ) & ~06000;
     }
 
-    chmod S_IMODE( $mode ), $target or die( sprintf( "Failed to preserve permissions for '%s': %s", $target, $! ));
+    chmod S_IMODE( $mode ), $dest or die( sprintf( "Failed to set permissions for '%s': %s", $dest, $! ));
     $self;
 }
 
-=item move( $target )
+=item move( $dest )
 
- Move this file to the given target
+ Move this file to the given destination
 
- Param string target Target
+ At this time, only regular and symlink files are considered. Other files are
+ silently ignored.
+ 
+ A specific treatment is applied on symlinks as thir target paths can be
+ relative to their current location. if so, those are simply recreated from
+ using their new location as base directory for generating a new relative
+ target path. If the target path is an absolute path, these are treaded as
+ regular files.
+
+ Note that even if File::Copy::move() is able to move directories, we raise an
+ error in case $self->{'filename'} refers to a directory. If you need to move
+ a directory, make use of iMSCP::Dir::move() instead.
+
+ Param string $dest Destination
  Return self, die on failure
 
 =cut
 
 sub move
 {
-    my ($self, $target) = @_;
+    my ($self, $dest) = @_;
 
-    defined $target or croak( '$target parameter is missing.' );
+    defined $dest or croak( 'dest parameter is missing.' );
 
-    -l $self->{'filename'} || !-d _ or die(
-        sprintf( "Couldn't move '%s' to '%s': %s is not a file", $self->{'dirname'}, $target, $self->{'dirname'} )
-    );
+    my (@sst) = lstat $self->{'filename'} or croak( sprintf( "Failed to stat '%s': %s", $self->{'filename'}, $! ));
+    !S_ISDIR( $sst[2] ) or die( sprintf( "Failed to move '%s' to '%s': Not a file", $self->{'filename'}, $dest, $self->{'filename'} ));
 
-    if ( File::Copy::mv( $self->{'filename'}, $target ) ) {
-        # Update the 'filename' attribute to make us able to continue working with
-        # this file regardless of its new location.
-        $self->{'filename'} = -d $target ? File::Spec->catfile( $target, basename( $self->{'filename'} )) : $target;
+    # Files other than symlinks and regular files are ignored silently.
+    return $self unless S_ISLNK( $sst[2] ) || S_ISREG( $sst[2] );
+
+    my (@dst) = stat $dest;
+    @dst || $! == ENOENT or die( sprintf( "Failed to access '%s': %s", $dest, $! ));
+
+    if ( S_ISLNK( $sst[2] ) && !File::Spec->file_name_is_absolute( readlink $self->{'filename'} ) ) {
+        # Turn target relative path into absolute path
+        my $lnkTgtAbs = Cwd::abs_path( $self->{'filename'} );
+        # Guess base directory for new symlink location
+        my $lnkDir = @dst && S_ISDIR( $dst[2] ) ? $dest : File::Basename::dirname( $dest );
+        # Generate new target relative path
+        my $lnkTgtRel = File::Spec->abs2rel( $lnkTgtAbs, $lnkDir );
+
+        $dest = File::Spec->catfile( $dest, File::Basename::basename( $self->{'filename'} )) if @dst && S_ISDIR( $dst[2] );
+
+        symlink $lnkTgtRel, $dest or die( sprintf( "Failed to move '%s' to '%s': %s", $self->{'filename'}, $dest, $! ));
+        unlink $self->{'filename'};
+
+        # Update $self->{'filename'}' to make us able to continue working with this file regardless of its new location.
+        $self->{'filename'} = $dest;
         return $self;
-    };
+    }
 
-    die( sprintf( "Failed to move '%s' to '%s': %s", $self->{'filename'}, $target, $! ));
+    File::Copy::mv( $self->{'filename'}, $dest ) or die( sprintf( "Failed to move '%s' to '%s': %s", $self->{'filename'}, $dest, $! ));
+
+    # Update $self->{'filename'}' to make us able to continue working with this file regardless of its new location.
+    $self->{'filename'} = @dst && S_ISDIR( $dst[2] ) ? File::Spec->catfile( $dest, File::Basename::basename( $self->{'filename'} )) : $dest;
+    $self;
 }
 
 =back
@@ -356,13 +402,13 @@ sub _init
     $self;
 }
 
-=item STRINGIFY()
+=item __toString()
 
- Return string representation of this object, that is the filename.
+ Return string representation of this object, that is the value of $self->{'filename'}
 
 =cut
 
-sub STRINGIFY
+sub __toString
 {
     $_[0]->{'filename'};
 }
