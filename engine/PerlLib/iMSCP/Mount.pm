573ce29a1b9d14dfae02ebb82e341ea903b010f7
Laurent Declercq <l.declercq@nuxwin.com>
2018-02-24 17:59:03 +0100

    Review: iMSCP::Mount library

--- engine/PerlLib/iMSCP/Mount.pm
+++ engine/PerlLib/iMSCP/Mount.pm
@@ -5,21 +5,21 @@
 =cut
 
 # i-MSCP - internet Multi Server Control Panel
-# Copyright (C) 2010-2018 by Laurent Declercq <l.declercq@nuxwin.com>
+# Copyright (C) 2010-2018 Laurent Declercq <l.declercq@nuxwin.com>
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 package iMSCP::Mount;
 
@@ -31,103 +31,74 @@ use File::Spec;
 use File::stat ();
 use iMSCP::Debug qw/ debug /;
 use iMSCP::File;
-use iMSCP::Syscall;
+use iMSCP::H2ph;
 use Scalar::Defer;
 use parent 'Exporter';
 
 our @EXPORT_OK = qw/ addMountEntry getMounts isMountpoint mount setPropagationFlag removeMountEntry umount /;
-
-# These are the fs-independent mount-flags (see sys/mount.h)
-# See http://man7.org/linux/man-pages/man2/mount.2.html for a description of these flags
-use constant {
-    # These are the fs-independent mount-flags
-    MS_RDONLY      => 1, # Mount read-only.
-    MS_NOSUID      => 2, # Ignore suid and sgid bits.
-    MS_NODEV       => 4, # Disallow access to device special files.
-    MS_NOEXEC      => 8, # Disallow program execution.
-    MS_SYNCHRONOUS => 16, # Writes are synced at once.
-    MS_REMOUNT     => 32, # Alter flags of a mounted FS.
-    MS_MANDLOCK    => 64, # Allow mandatory locks on an FS.
-    MS_DIRSYNC     => 128, # Directory modifications are synchronous.
-    MS_NOATIME     => 1024, # Do not update access times.
-    MS_NODIRATIME  => 2048, # Do not update directory access times.
-    MS_BIND        => 4096, # Bind directory at different place.
-    MS_MOVE        => 8192, # Move a subtree.
-    MS_REC         => 16384, # Recursive loopback.
-    MS_SILENT      => 32768, # Be quiet.
-    MS_POSIXACL    => 1 << 16, # VFS does not apply the umask.
-    MS_UNBINDABLE  => 1 << 17, # Change to unbindable.
-    MS_PRIVATE     => 1 << 18, # Change to private.
-    MS_SLAVE       => 1 << 19, # Change to slave.
-    MS_SHARED      => 1 << 20, # Change to shared.
-    MS_RELATIME    => 1 << 21, # Update atime relative to mtime/ctime.
-    MS_KERNMOUNT   => 1 << 22, # This is a kern_mount call.
-    MS_I_VERSION   => 1 << 23, # Update inode I_version field.
-    MS_STRICTATIME => 1 << 24, # Always perform atime updates.
-    MS_LAZYTIME    => 1 << 25 # Update the time lazily. (since Linux 4.0)
-};
-use constant {
-    # Flags that can be altered by MS_REMOUNT (see sys/mount.h)
-    MS_RMT_MASK     => ( MS_RDONLY | MS_SYNCHRONOUS | MS_MANDLOCK | MS_I_VERSION ),
-
-    # Magic mount flag number. Has to be or-ed to the flag values. (see sys/mount.h)
-    MS_MGC_VAL      => 0xc0ed0000, # Magic flag number to indicate "new" flags
-    MS_MGC_MSK      => 0xffff0000, # Magic flag number mask
-
-    # Possible value for FLAGS parameter of `umount2' (see sys/mount.h)
-    MNT_FORCE       => 1,
-    MNT_DETACH      => 2,
-    MNT_EXPIRE      => 4,
-    UMOUNT_NOFOLLOW => 8
-};
+our %EXPORT_TAGS = ( all => \@EXPORT_OK );
 
 # Mount options
-# List taken from libmount/src/optmap.c (util-linux 2.25.2)
 my %MOUNT_FLAGS = (
     defaults      => sub { 0 },
-    bind          => sub { $_[0] | MS_BIND },
-    rbind         => sub { $_[0] | MS_BIND | MS_REC },
-    ro            => sub { $_[0] | MS_RDONLY },
-    rw            => sub { $_[0] & ~MS_RDONLY },
-    exec          => sub { $_[0] & ~MS_NOEXEC },
-    noexec        => sub { $_[0] | MS_NOEXEC },
-    suid          => sub { $_[0] & ~MS_NOSUID },
-    nosuid        => sub { $_[0] | MS_NOSUID },
-    dev           => sub { $_[0] & ~MS_NODEV },
-    nodev         => sub { $_[0] | MS_NODEV },
-    sync          => sub { $_[0] | MS_SYNCHRONOUS },
-    async         => sub { $_[0] & ~MS_SYNCHRONOUS },
-    dirsync       => sub { $_[0] | MS_DIRSYNC },
-    remount       => sub { $_[0] | MS_REMOUNT },
-    silent        => sub { $_[0] | MS_SILENT },
-    loud          => sub { $_[0] & ~MS_SILENT },
-    move          => sub { $_[0] | MS_MOVE },
-    mand          => sub { $_[0] | MS_MANDLOCK },
-    nomand        => sub { $_[0] & ~MS_MANDLOCK },
-    atime         => sub { $_[0] & ~MS_NOATIME },
-    noatime       => sub { $_[0] | MS_NOATIME },
-    iversion      => sub { $_[0] | MS_I_VERSION },
-    noiversion    => sub { $_[0] & ~MS_I_VERSION },
-    diratime      => sub { $_[0] & ~MS_NODIRATIME },
-    nodiratime    => sub { $_[0] | MS_NODIRATIME },
-    relatime      => sub { $_[0] | MS_RELATIME },
-    norelatime    => sub { $_[0] & ~MS_RELATIME },
-    strictatime   => sub { $_[0] | MS_STRICTATIME },
-    nostrictatime => sub { $_[0] & ~MS_STRICTATIME },
-    lazytime      => sub { $_[0] & ~MS_LAZYTIME }
+
+    ro            => sub { $_[0] | &iMSCP::H2ph::MS_RDONLY },
+    rw            => sub { $_[0] & ~&iMSCP::H2ph::MS_RDONLY },
+    exec          => sub { $_[0] & ~&iMSCP::H2ph::MS_NOEXEC },
+    noexec        => sub { $_[0] | &iMSCP::H2ph::MS_NOEXEC },
+    suid          => sub { $_[0] & ~&iMSCP::H2ph::MS_NOSUID },
+    nosuid        => sub { $_[0] | &iMSCP::H2ph::MS_NOSUID },
+    dev           => sub { $_[0] & ~&iMSCP::H2ph::MS_NODEV },
+    nodev         => sub { $_[0] | &iMSCP::H2ph::MS_NODEV },
+
+    sync          => sub { $_[0] | &iMSCP::H2ph::MS_SYNCHRONOUS },
+    async         => sub { $_[0] & ~&iMSCP::H2ph::MS_SYNCHRONOUS },
+
+    dirsync       => sub { $_[0] | &iMSCP::H2ph::MS_DIRSYNC },
+    remount       => sub { $_[0] | &iMSCP::H2ph::MS_REMOUNT },
+    bind          => sub { $_[0] | &iMSCP::H2ph::MS_BIND },
+    rbind         => sub { $_[0] | &iMSCP::H2ph::MS_BIND | &iMSCP::H2ph::MS_REC },
+
+    sub           => defined( &iMSCP::H2ph::MS_NOSUB ) ? sub { $_[0] & ~&iMSCP::H2ph::MS_NOSUB } : sub { $_[0] },
+    nosub         => defined( &iMSCP::H2ph::MS_NOSUB ) ? sub { $_[0] | &iMSCP::H2ph::MS_NOSUB } : sub { $_[0] },
+
+    silent        => defined( &iMSCP::H2ph::MS_SILENT ) ? sub { $_[0] | &iMSCP::H2ph::MS_SILENT } : sub { $_[0] },
+    loud          => defined( &iMSCP::H2ph::MS_SILENT ) ? sub { $_[0] & ~&iMSCP::H2ph::MS_SILENT } : sub { $_[0] },
+
+    mand          => defined( &iMSCP::H2ph::MS_MANDLOCK ) ? sub { $_[0] | &iMSCP::H2ph::MS_MANDLOCK } : sub { $_[0] },
+    nomand        => defined( &iMSCP::H2ph::MS_MANDLOCK ) ? sub { $_[0] & ~&iMSCP::H2ph::MS_MANDLOCK } : sub { $_[0] },
+
+    atime         => defined( &iMSCP::H2ph::MS_NOATIME ) ? sub { $_[0] & ~&iMSCP::H2ph::MS_NOATIME } : sub { $_[0] },
+    noatime       => defined( &iMSCP::H2ph::MS_NOATIME ) ? sub { $_[0] | &iMSCP::H2ph::MS_NOATIME } : sub { $_[0] },
+
+    iversion      => defined( &iMSCP::H2ph::MS_I_VERSION ) ? sub { $_[0] | &iMSCP::H2ph::MS_I_VERSION } : sub { $_[0] },
+    noiversion    => defined( &iMSCP::H2ph::MS_I_VERSION ) ? sub { $_[0] & ~&iMSCP::H2ph::MS_I_VERSION } : sub { $_[0] },
+
+    diratime      => defined( &iMSCP::H2ph::MS_NODIRATIME ) ? sub { $_[0] & ~&iMSCP::H2ph::MS_NODIRATIME } : sub { $_[0] },
+    nodiratime    => defined( &iMSCP::H2ph::MS_NODIRATIME ) ? sub { $_[0] | &iMSCP::H2ph::MS_NODIRATIME } : sub { $_[0] },
+
+    relatime      => defined( &iMSCP::H2ph::MS_RELATIME ) ? sub { $_[0] | &iMSCP::H2ph::MS_RELATIME } : sub { $_[0] },
+    norelatime    => defined( &iMSCP::H2ph::MS_RELATIME ) ? sub { $_[0] & ~&iMSCP::H2ph::MS_RELATIME } : sub { $_[0] },
+
+    strictatime   => defined( &iMSCP::H2ph::MS_STRICTATIME ) ? sub { $_[0] | &iMSCP::H2ph::MS_STRICTATIME } : sub { $_[0] },
+    nostrictatime => defined( &iMSCP::H2ph::MS_STRICTATIME ) ? sub { $_[0] & ~&iMSCP::H2ph::MS_STRICTATIME } : sub { $_[0] },
+
+    lazytime      => defined( &iMSCP::H2ph::MS_LAZYTIME ) ? sub { $_[0] | &iMSCP::H2ph::MS_LAZYTIME } : sub { $_[0] },
+    nolazytime    => defined( &iMSCP::H2ph::MS_LAZYTIME ) ? sub { $_[0] & ~&iMSCP::H2ph::MS_LAZYTIME } : sub { $_[0] },
+
+    move          => sub { $_[0] | &iMSCP::H2ph::MS_MOVE }
 );
 
 # Propagation flags
-# List taken from libmount/src/optmap.c (util-linux 2.25.2)
 my %PROPAGATION_FLAGS = (
-    unbindable  => sub { $_[0] | MS_UNBINDABLE },
-    runbindable => sub { $_[0] | MS_UNBINDABLE | MS_REC },
-    private     => sub { $_[0] | MS_PRIVATE },
-    rprivate    => sub { $_[0] | MS_PRIVATE | MS_REC },
-    slave       => sub { $_[0] | MS_SLAVE },
-    rslave      => sub { $_[0] | MS_SLAVE | MS_REC },
-    shared      => sub { $_[0] | MS_SHARED },
-    rshared     => sub { $_[0] | MS_SHARED | MS_REC }
+    unbindable  => sub { $_[0] | &iMSCP::H2ph::MS_UNBINDABLE },
+    runbindable => sub { $_[0] | &iMSCP::H2ph::MS_UNBINDABLE | &iMSCP::H2ph::MS_REC },
+    private     => sub { $_[0] | &iMSCP::H2ph::MS_PRIVATE },
+    rprivate    => sub { $_[0] | &iMSCP::H2ph::MS_PRIVATE | &iMSCP::H2ph::MS_REC },
+    slave       => sub { $_[0] | &iMSCP::H2ph::MS_SLAVE },
+    rslave      => sub { $_[0] | &iMSCP::H2ph::MS_SLAVE | &iMSCP::H2ph::MS_REC },
+    shared      => sub { $_[0] | &iMSCP::H2ph::MS_SHARED },
+    rshared     => sub { $_[0] | &iMSCP::H2ph::MS_SHARED | &iMSCP::H2ph::MS_REC }
 );
 
 # Lazy-load mount entries
@@ -174,12 +145,10 @@ sub getMounts
  propagation type of an existing mount
 
  Param hashref \%fields Hash describing filesystem to mount:
-  - fs_spec         : Field describing the block special device or remote
-                      filesystem to be mounted
+  - fs_spec         : Field describing the block special device or remote filesystem to be mounted
   - fs_file         : Field describing the mount point for the filesystem
   - fs_vfstype      : Field describing the type of the filesystem
-  - fs_mntops       : Field describing the mount options associated with the
-                      filesystem
+  - fs_mntops       : Field describing the mount options associated with the filesystem
   - ignore_failures : Flag allowing to ignore mount operation failures
  Return void, die on failure
 
@@ -190,8 +159,8 @@ sub mount( $ )
     my ($fields) = @_;
     $fields = {} unless defined $fields && ref $fields eq 'HASH';
 
-    for ( qw/ fs_spec fs_file fs_vfstype fs_mntops / ) {
-        defined $fields->{$_} or croak( sprintf( "%s field not defined", $_ ));
+    for my $field( qw/ fs_spec fs_file fs_vfstype fs_mntops / ) {
+        defined $fields->{$field} or croak( sprintf( "%s field not defined", $field ));
     }
 
     force $MOUNTS; # Force loading of mount entries if not already done
@@ -202,11 +171,11 @@ sub mount( $ )
     debug( "$fsSpec $fsFile $fsVfstype $fields->{'fs_mntops'}" );
 
     my ($mflags, $pflags, $data) = _parseOptions( $fields->{'fs_mntops'} );
-    $mflags |= MS_MGC_VAL unless $mflags & MS_MGC_MSK;
+    $mflags |= &iMSCP::H2ph::MS_MGC_VAL unless $mflags & &iMSCP::H2ph::MS_MGC_MSK;
 
     my @mountArgv;
 
-    if ( $mflags & MS_BIND ) {
+    if ( $mflags & &iMSCP::H2ph::MS_BIND ) {
         # Create a bind mount or remount an existing bind mount
         push @mountArgv, [ $fsSpec, $fsFile, $fsVfstype, $mflags, $data ];
 
@@ -214,8 +183,8 @@ sub mount( $ )
         # than MS_BIND and MS_REC, schedule an additional mount(2) call to
         # change mountflags on existing mount. This is needed since mountflags
         # other than MS_BIND and MS_REC are ignored in first call.
-        if ( !( $mflags & MS_REMOUNT ) && ( $mflags & ~( MS_BIND | MS_REC ) ) ) {
-            push @mountArgv, [ $fsSpec, $fsFile, $fsVfstype, MS_REMOUNT | $mflags, $data ];
+        if ( !( $mflags & &iMSCP::H2ph::MS_REMOUNT ) && ( $mflags & ~( &iMSCP::H2ph::MS_BIND | &iMSCP::H2ph::MS_REC ) ) ) {
+            push @mountArgv, [ $fsSpec, $fsFile, $fsVfstype, &iMSCP::H2ph::MS_REMOUNT | $mflags, $data ];
         }
     } elsif ( $fsSpec ne 'none' ) {
         # Create a new mount or remount an existing mount
@@ -226,13 +195,13 @@ sub mount( $ )
     push @mountArgv, [ 'none', $fsFile, 0, $pflags, 0 ] if $pflags;
 
     # Process the mount(2) calls
-    for ( @mountArgv ) {
-        ( syscall( &iMSCP::Syscall::SYS_mount, @{$_} ) == 0 || $fields->{'ignore_failures'} ) or die(
-            sprintf( 'Error while executing mount(%s): %s', join( ', ', @{$_} ), $! || 'Unknown error' )
+    for my $mountArg( @mountArgv ) {
+        ( syscall( &iMSCP::H2ph::SYS_mount, @{$mountArg} ) == 0 || $fields->{'ignore_failures'} ) or die(
+            sprintf( 'Error while executing mount(%s): %s', join( ', ', @{$mountArg} ), $! || 'Unknown error' )
         );
     }
 
-    $MOUNTS->{$fsFile}++ unless $mflags & MS_REMOUNT;
+    $MOUNTS->{$fsFile}++ unless $mflags & &iMSCP::H2ph::MS_REMOUNT;
 }
 
 =item umount( $fsFile [, $recursive = TRUE ] )
@@ -243,8 +212,7 @@ sub mount( $ )
  (or directory) will be umounted.
 
  Param string $fsFile Mount point of file system to umount
- Param bool $recursive OPTIONAL Flag indicating whether or not umount operation
-                       must be recursive
+ Param bool $recursive OPTIONAL Flag indicating whether or not umount operation must be recursive
  Return void, die on failure
 
 =cut
@@ -266,7 +234,7 @@ sub umount( $;$ )
         do {
             debug( $fsFile );
 
-            ( syscall( &iMSCP::Syscall::SYS_umount2, $fsFile, MNT_DETACH ) == 0 || $!{'EINVAL'} || $!{'ENOENT'} ) or die(
+            ( syscall( &iMSCP::H2ph::SYS_umount2, $fsFile, &iMSCP::H2ph::MNT_DETACH ) == 0 || $! == EINVAL || $! == ENOENT ) or die(
                 sprintf( "Error while executing umount(%s): %s", $fsFile, $! || 'Unknown error' )
             );
 
@@ -276,18 +244,18 @@ sub umount( $;$ )
         return;
     }
 
-    for ( reverse sort keys %{$MOUNTS} ) {
-        next unless /^\Q$fsFile\E(\/|$)/;
+    for my $mount( reverse sort keys %{$MOUNTS} ) {
+        next unless $mount =~ /^\Q$fsFile\E(\/|$)/;
 
         do {
-            debug( $_ );
+            debug( $mount );
 
-            ( syscall( &iMSCP::Syscall::SYS_umount2, $_, MNT_DETACH ) == 0 || $!{'EINVAL'} || $!{'ENOENT'} ) or die(
-                sprintf( "Error while executing umount(%s): %s", $_, $! || 'Unknown error' )
+            ( syscall( &iMSCP::H2ph::SYS_umount2, $mount, &iMSCP::H2ph::MNT_DETACH ) == 0 || $! == EINVAL || $! == ENOENT ) or die(
+                sprintf( "Error while executing umount(%s): %s", $mount, $! || 'Unknown error' )
             );
 
-            ( $MOUNTS->{$_} > 1 ) ? $MOUNTS->{$_}-- : delete $MOUNTS->{$_};
-        } while $MOUNTS->{$_};
+            ( $MOUNTS->{$mount} > 1 ) ? $MOUNTS->{$mount}-- : delete $MOUNTS->{$mount};
+        } while $MOUNTS->{$mount};
     }
 
     0;
@@ -318,7 +286,7 @@ sub setPropagationFlag($;$)
     $pflag or croak( 'Invalid propagation flags' );
 
     my $src = 'none';
-    syscall( &iMSCP::Syscall::SYS_mount, $src, $fsFile, 0, $pflag, 0 ) == 0 or die(
+    syscall( &iMSCP::H2ph::SYS_mount, $src, $fsFile, 0, $pflag, 0 ) == 0 or die(
         sprintf( 'Error while changing propagation flag on %s: %s', $fsFile, $! || 'Unknown error' )
     );
 }
@@ -389,7 +357,7 @@ sub removeMountEntry( $;$ )
 
     defined $entry or croak( '$entry parameter is not defined' );
 
-    my $fileContent = ( $iMSCP_FSTAB_FH ||= iMSCP::File->new( filename => "$main::imscpConfig{'CONF_DIR'}/mounts/mounts.conf" ) )->getAsRef();
+    my $fileContent = ( $iMSCP_FSTAB_FH ||= iMSCP::File->new( filename => "$::imscpConfig{'CONF_DIR'}/mounts/mounts.conf" ) )->getAsRef();
     $entry = quotemeta( $entry ) unless ref $entry eq 'Regexp';
     ${$fileContent} =~ s/^$entry\n//gm;
     $iMSCP_FSTAB_FH->save() if $saveFile;
@@ -412,23 +380,23 @@ sub removeMountEntry( $;$ )
 
 sub _parseOptions( $ )
 {
-    my $options = shift;
+    my ($options) = @_;
 
     # Turn options string into option list
     my @options = split /[\s,]+/, $options;
 
     # Parse mount flags (excluding any propagation flag)
     my ($mflags, @roptions) = ( 0 );
-    for ( @options ) {
-        push( @roptions, $_ ) && next unless exists $MOUNT_FLAGS{$_};
-        $mflags = $MOUNT_FLAGS{$_}->( $mflags );
+    for my $option( @options ) {
+        push( @roptions, $option ) && next unless exists $MOUNT_FLAGS{$option};
+        $mflags = $MOUNT_FLAGS{$option}->( $mflags );
     }
 
     # Parse propagation flags
     my ($pflags, @data) = ( 0 );
-    for ( @roptions ) {
-        push( @data, $_ ) && next unless exists $PROPAGATION_FLAGS{$_};
-        $pflags = $PROPAGATION_FLAGS{$_}->( $pflags );
+    for my $option( @roptions ) {
+        push( @data, $option ) && next unless exists $PROPAGATION_FLAGS{$option};
+        $pflags = $PROPAGATION_FLAGS{$option}->( $pflags );
     }
 
     ( $mflags, $pflags, ( @data ) ? join ',', @data : 0 );
