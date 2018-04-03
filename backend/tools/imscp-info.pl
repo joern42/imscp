d917ac2ffea6b8c6e18f85b180f1ee3f042206af
Laurent Declercq <l.declercq@nuxwin.com>
2018-04-04 01:42:35 +0200

    Enhancement: Added package info (imscp-info.pl) Small fixes

--- backend/tools/imscp-info.pl
+++ backend/tools/imscp-info.pl
@@ -29,30 +29,42 @@
 
 use strict;
 use warnings;
-use FindBin;
+use autouse 'iMSCP::Debug' => qw/ output /;
+use Class::Autouse qw/ :nostat iMSCP::Packages iMSCP::Servers JSON /;
 use File::Basename;
+use FindBin;
 use lib "$FindBin::Bin/../PerlLib";
 use iMSCP::Boolean;
 use iMSCP::Bootstrapper;
-use iMSCP::Debug qw/ output /;
 use iMSCP::Getopt;
-use iMSCP::Servers;
-use JSON qw/ to_json /;
 
 iMSCP::Getopt->parseNoDefault( sprintf( 'Usage: perl %s [OPTION]...', basename( $0 )) . qq{
 
 Display information about i-MSCP instance.
 
+By default all information are shown. Use the display options below to restrict
+list of information. You can cumul the display options.
+
 OPTIONS:
- -v,    --version-only  Display i-MSCP version info only.
- -s,    --system-only   Display i-MSCP system info only.
+ -v,    --release       Display release info.
+ -s,    --servers       Display servers info.
+ -p,    --packages      Display packages info.
+ -d,    --daemon        Displat daemon info.
  -j,    --json          Display output in JSON format.
- -p,    --pretty        Enable JSON pretty print.},
-    'version-only|v' => \&iMSCP::Getopt::versionOnly,
-    'server-only|s'  => \&iMSCP::Getopt::serverOnly,
-    'json|j'         => \&iMSCP::Getopt::json,
-    'pretty|p'       => \&iMSCP::Getopt::pretty
+ -t,    --pretty        Enable JSON pretty print.
+ -n,    --noansi        Disable ainsi output.
+ -e,    --noheaders     Disable headers.},
+    'release|r'   => \&iMSCP::Getopt::release,
+    'servers|s'   => \&iMSCP::Getopt::servers,
+    'packages|p'  => \&iMSCP::Getopt::packages,
+    'daemon|d'    => \&iMSCP::Getopt::daemon,
+    'json|j'      => \&iMSCP::Getopt::json,
+    'pretty|t'    => \&iMSCP::Getopt::pretty,
+    'noansi|n'    => \&iMSCP::Getopt::noansi,
+    'noheaders|e' => \&iMSCP::Getopt::noheaders
 );
+
+my $nodisplayopts = !iMSCP::Getopt->release && !iMSCP::Getopt->servers && !iMSCP::Getopt->packages && !iMSCP::Getopt->daemon;
 
 iMSCP::Bootstrapper->getInstance()->boot( {
     config_readonly => TRUE,
@@ -62,79 +74,137 @@ iMSCP::Bootstrapper->getInstance()->boot( {
 } );
 
 iMSCP::Getopt->debug( FALSE );
-
-if ( iMSCP::Getopt->versionOnly && iMSCP::Getopt->serverOnly ) {
-    print "\nThe --version-only and --system-only options are mutually exclusive\n";
-    iMSCP::Getopt->showUsage();
-}
+iMSCP::Getopt->context( 'installer' );
 
 my $json = {} if iMSCP::Getopt->json;
 
-unless ( iMSCP::Getopt->serverOnly ) {
+if ( $nodisplayopts || iMSCP::Getopt->release ) {
     if ( iMSCP::Getopt->json ) {
         $json->{'build_date'} = $::imscpConfig{'BuildDate'} || 'Unreleased';
         $json->{'version'} = $::imscpConfig{'Version'};
         $json->{'codename'} = $::imscpConfig{'CodeName'};
         $json->{'plugin_api'} = $::imscpConfig{'PluginApi'};
     } else {
+        print <<'EOF' unless iMSCP::Getopt->noheaders;
+
+###############################################################################
+###                              Release Info                               ###
+###############################################################################
+
+EOF
+        print output "Build date            : @{ [ $::imscpConfig{'BuildDate'} || 'Unreleased' ] }", 'info';
+        print output "Version               : $::imscpConfig{'Version'}", 'info';
+        print output "Codename              : $::imscpConfig{'CodeName'}", 'info';
+        print output "Plugin API            : $::imscpConfig{'PluginApi'}", 'info';
+    }
+}
+
+if ( $nodisplayopts || iMSCP::Getopt->daemon ) {
+    unless ( iMSCP::Getopt->json || iMSCP::Getopt->noheaders ) {
         print <<'EOF';
 
-#################################################################
-###                    i-MSCP Version Info                    ###
-#################################################################
+###############################################################################
+###                               Daemon Info                               ###
+###############################################################################
 
 EOF
-        print output "Build date                       : @{ [ $::imscpConfig{'BuildDate'} || 'Unreleased' ] }", 'info';
-        print output "Version                          : $::imscpConfig{'Version'}", 'info';
-        print output "Codename                         : $::imscpConfig{'CodeName'}", 'info';
-        print output "Plugin API                       : $::imscpConfig{'PluginApi'}", 'info';
+        print output "Daemon type           : $::imscpConfig{'DAEMON_TYPE'}", 'info';
+    } else {
+        $json->{'daemon_type'} = $::imscpConfig{'DAEMON_TYPE'};
     }
 }
 
-if ( iMSCP::Getopt->versionOnly ) {
-    print to_json( $json, { utf8 => TRUE, pretty => iMSCP::Getopt->pretty } ) if iMSCP::Getopt->json;
-    exit;
-}
+if ( $nodisplayopts || iMSCP::Getopt->servers ) {
+    unless ( iMSCP::Getopt->json || iMSCP::Getopt->noheaders ) {
+        print <<'EOF';
 
-unless ( iMSCP::Getopt->json ) {
-    print <<'EOF';
-
-#################################################################
-###                    i-MSCP System Info                     ###
-#################################################################
+###############################################################################
+###                               Servers Info                              ###
+###############################################################################
 
 EOF
-
-    print output "Daemon type for backend requests : $::imscpConfig{'DAEMON_TYPE'}", 'info';
-    print "\n";
-} else {
-    $json->{'daemon_type'} = $::imscpConfig{'DAEMON_TYPE'};
-}
-
-for my $server ( iMSCP::Servers->getInstance()->getListWithFullNames() ) {
-    my $srvInstance = $server->factory();
-
-    if ( iMSCP::Getopt->json ) {
-        $json->{'servers'}->{$server} = {
-            implementation => ref $srvInstance,
-            version        => $srvInstance->getImplVersion(),
-            internal_name  => $srvInstance->getServerName(),
-            human_name     => $srvInstance->getHumanServerName(),
-            priority       => $srvInstance->getPriority()
-        };
-        next;
     }
 
-    print output "Server                           : $server", 'info';
-    print output "Server implementation            : @{ [ ref $srvInstance ] }", 'info';
-    print output "Server implementation version    : @{ [ $srvInstance->getImplVersion() ] }", 'info';
-    print output "Server name for internal use     : @{ [ $srvInstance->getServerName() ] }", 'info';
-    print output "Server human name                : @{ [ $srvInstance->getHumanServerName() ] }", 'info';
-    print output "Server priority for processing   : @{ [ $srvInstance->getPriority() ] }", 'info';
-    print "\n";
+    for ( iMSCP::Servers->getInstance()->getList() ) {
+        my $srvInstance = $_->factory();
+
+        if ( iMSCP::Getopt->json ) {
+            $json->{'servers'}->{$_} = {
+                implementation => ref $srvInstance,
+                version        => $srvInstance->getServerImplVersion(),
+                internal_name  => $srvInstance->getServerName(),
+                human_name     => $srvInstance->getServerHumanName(),
+                priority       => $srvInstance->getServerPriority()
+            };
+            next;
+        }
+
+        print output "Server                : $_", 'info';
+        print output "Server implementation : @{ [ ref $srvInstance ] }", 'info';
+        print output "Server version        : @{ [ $srvInstance->getServerImplVersion() ] }", 'info';
+        print output "Server name           : @{ [ $srvInstance->getServerName() ] }", 'info';
+        print output "Server human name     : @{ [ $srvInstance->getServerHumanName() ] }", 'info';
+        print output "Server priority       : @{ [ $srvInstance->getServerPriority() ] }", 'info';
+        print "\n" unless iMSCP::Getopt->noheaders;
+    }
 }
 
-print to_json( $json, { utf8 => TRUE, pretty => iMSCP::Getopt->pretty } ) if iMSCP::Getopt->json;
+if ( $nodisplayopts || iMSCP::Getopt->packages ) {
+    unless ( iMSCP::Getopt->json || iMSCP::Getopt->noheaders ) {
+        print <<'EOF';
+###############################################################################
+###                              Packages Info                              ###
+###############################################################################
+
+EOF
+    }
+
+    for ( iMSCP::Packages->getInstance()->getList() ) {
+        my $pkgInstance = $_->getInstance();
+
+        if ( $pkgInstance->isa( 'iMSCP::Packages::AbstractCollection' ) ) {
+            for ( $pkgInstance->getCollection() ) {
+                if ( iMSCP::Getopt->json ) {
+                    $json->{'packages'}->{ref $_} = {
+                        version       => $_->getPackageImplVersion(),
+                        internal_name => $_->getPackageName(),
+                        human_name    => $_->getPackageHumanName(),
+                        priority      => $_->getPackagePriority()
+                    };
+                    next;
+                }
+
+                print output "Package               : @{ [ ref $_ ] }", 'info';
+                print output "Package version       : @{ [ $_->getPackageImplVersion() ] }", 'info';
+                print output "Package name          : @{ [ $_->getPackageName() ] }", 'info';
+                print output "Package human name    : @{ [ $_->getPackageHumanName() ] }", 'info';
+                print output "Package priority      : @{ [ $_->getPackagePriority() ] }", 'info';
+                print "\n";
+
+            }
+        } else {
+            if ( iMSCP::Getopt->json ) {
+                $json->{'packages'}->{$_} = {
+                    implementation => ref $pkgInstance,
+                    version        => $pkgInstance->getPackageImplVersion(),
+                    internal_name  => $pkgInstance->getPackageName(),
+                    human_name     => $pkgInstance->getPackageHumanName(),
+                    priority       => $pkgInstance->getPackagePriority()
+                };
+                next;
+            }
+
+            print output "Package               : $_", 'info';
+            print output "Package version       : @{ [ $pkgInstance->getPackageImplVersion() ] }", 'info';
+            print output "Package name          : @{ [ $pkgInstance->getPackageName() ] }", 'info';
+            print output "Package human name    : @{ [ $pkgInstance->getPackageHumanName() ] }", 'info';
+            print output "Package priority      : @{ [ $pkgInstance->getPackagePriority() ] }", 'info';
+            print "\n";
+        }
+    }
+}
+
+print JSON->new()->utf8()->pretty( iMSCP::Getopt->pretty )->encode( $json ) if iMSCP::Getopt->json;
 
 =head1 AUTHOR
 
