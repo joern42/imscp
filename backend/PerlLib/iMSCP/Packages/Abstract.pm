47d20ba92b36f5ecd960a8dc2b0b6a7c0a893b08
Geoff Maciolek <GeoffMaciolek@gmail.com>
2018-04-03 05:19:59 +0200

    Added: Abstract implementation for i-MSCP packages Added: Abstract collection implementation for i-MSCP packages

---
+++ backend/PerlLib/iMSCP/Packages/Abstract.pm
@@ -0,0 +1,626 @@
+=head1 NAME
+
+ iMSCP::Packages::Abstract - Abstract implementation for i-MSCP packages
+
+=cut
+
+# i-MSCP - internet Multi Server Control Panel
+# Copyright (C) 2010-2018 Laurent Declercq <l.declercq@nuxwin.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+package iMSCP::Packages::Abstract;
+
+use strict;
+use warnings;
+use Class::Autouse qw/ :nostat iMSCP::DistPackageManager /;
+use Carp qw/ confess croak /;
+use File::Basename;
+use File::Spec;
+use iMSCP::Boolean;
+use iMSCP::Config;
+use iMSCP::Database;
+use iMSCP::Debug qw/ debug getMessageByType /;
+use iMSCP::EventManager;
+use iMSCP::File;
+use iMSCP::Getopt;
+use iMSCP::TemplateParser qw/ processByRef /;
+use parent 'iMSCP::Common::Singleton';
+
+=head1 DESCRIPTION
+
+ This class provides an abstract implementation for the i-MSCP packages.
+
+=head1 CLASS METHODS
+
+=over 4
+
+=item getPackagePriority( )
+
+ Return the package priority
+  
+ The package priority determines the priority at which the package will be
+ treated by the installer, the backend, and some other scripts.
+
+ Return int Package priority
+
+=cut
+
+sub getPackagePriority
+{
+    0;
+}
+
+=back
+
+=head1 PUBLIC METHODS
+
+=over 4
+
+=item registerSetupListeners( )
+
+ Register setup event listeners
+ 
+ This method is called by the i-MSCP installer and reconfiguration script.
+ That is the place where event listeners for setup dialog *MUST* be registered.
+ 
+ Any package relying on i-MSCP setup dialog *MUST* implement this method.
+
+ Return void, die on failure
+
+=cut
+
+sub registerSetupListeners
+{
+    my ( $self ) = @_;
+}
+
+=item preinstall( )
+
+ Process the package pre-installation tasks
+ 
+ This method is called by the i-MSCP installer and reconfiguration script.
+
+ Any package requiring pre-installation tasks *SHOULD* implement this method.
+
+ Return void, die on failure
+
+=cut
+
+sub preinstall
+{
+    my ( $self ) = @_;
+}
+
+=item install( )
+
+ Process the package installation tasks
+
+ This method is called by the i-MSCP installer and reconfiguration script.
+ 
+ Any package requiring post-installation tasks *SHOULD* implement this method.
+
+ Return void, die on failure
+
+=cut
+
+sub install
+{
+    my ( $self ) = @_;
+}
+
+=item postinstall( )
+
+ Process package post-installation tasks
+
+ This method is called by the i-MSCP installer and reconfiguration script.
+ 
+ Any package requiring post-installation tasks *SHOULD* implement this method.
+
+ Return void, die on failure
+
+=cut
+
+sub postinstall
+{
+    my ( $self ) = @_;
+}
+
+=item preuninstall( )
+
+ Process the package pre-uninstallation tasks
+
+ This method is called by the i-MSCP installer and uninstaller.
+
+ Any package requiring pre-uninstallation tasks *SHOULD* implement this method.
+
+ Return void, die on failure
+
+=cut
+
+sub preuninstall
+{
+    my ( $self ) = @_;
+}
+
+=item uninstall( )
+
+ Process the package uninstallation tasks
+
+ This method is called by the i-MSCP installer and uninstaller.
+
+ Any package requiring uninstallation tasks *SHOULD* implement this method.
+
+ Return void, die on failure
+
+=cut
+
+sub uninstall
+{
+    my ( $self ) = @_;
+}
+
+=item postuninstall( )
+
+ Process the package post-uninstallation tasks
+
+ This method is called by the i-MSCP installer and uninstaller.
+
+ Any package requiring post-uninstallation tasks *SHOULD* implement this method.
+
+ Return void, die on failure
+
+=cut
+
+sub postuninstall
+{
+    my ( $self ) = @_;
+}
+
+=item setBackendPermissions( )
+
+ Set backend permissions
+
+ This method is called by the i-MSCP backend permission management script.
+
+ Any package relying on configuration files or scripts *SHOULD* implement this
+ method.
+
+ Return void, die on failure
+
+=cut
+
+sub setBackendPermissions
+{
+    my ( $self ) = @_;
+}
+
+=item setFrontendPermissions( )
+
+ Set frontEnd permissions
+
+ This method is called by the i-MSCP frontEnd permission management script.
+
+ Any package managing GUI files *SHOULD* implement this method.
+
+ Return void, die on failure
+
+=cut
+
+sub setFrontendPermissions
+{
+    my ( $self ) = @_;
+}
+
+=item getPackageName( )
+
+ Return internal CamelCase service name implemented by this package
+ 
+ Package name must follow CamelCase naming convention such as RoundCube
+ FrontEnd... See https://en.wikipedia.org/wiki/Camel_case
+ 
+ This method is primarily used for event names construct at runtime, and at
+ some other places where the internal package name must be showed such as in
+ the backend/tools/imscp-info.pl script.
+
+ Return string CamelCase service name
+
+=cut
+
+sub getPackageName
+{
+    my ( $self ) = @_;
+
+    die( sprintf( 'The %s class must implement the getPackageName() method', ref $self ));
+}
+
+=item getPackageHumanName( )
+
+ Return the humanized name of the service implemented by this package
+
+ For instance: RoundCube 1.3.4
+
+ Return string Humanized service name
+
+=cut
+
+sub getPackageHumanName
+{
+    my ( $self ) = @_;
+
+    die( sprintf( 'The %s class must implement the getPackageHumanName() method', ref $self ));
+}
+
+=item getPackageVersion()
+
+ Return the version of the service implemented by this package
+
+ Return string Service version
+
+=cut
+
+sub getPackageVersion
+{
+    my ( $self ) = @_;
+
+    die( sprintf( 'The %s class must implement the getPackageVersion() method', ref $self ));
+}
+
+=item getPackageImplVersion()
+
+ Return the implementation version of this package
+
+ Return string Package implementation version
+
+=cut
+
+sub getPackageImplVersion
+{
+    my ( $self ) = @_;
+
+    no strict 'refs';
+    ${ "@{ [ ref $self ] }::VERSION" } // '0.0.0';
+}
+
+=item dpkgPostInvokeTasks()
+
+ Process dpkg(1) post-invoke tasks
+
+ This method is called after each dpkg(1) invocation. This make it possible to
+ perform some maintenance tasks such as updating service versions.
+
+ Return void, die on failure
+
+=cut
+
+sub dpkgPostInvokeTasks
+{
+    my ( $self ) = @_;
+}
+
+=item getDistroPackages( )
+
+ Get list of distribution packages
+
+ Return list List of packages
+
+=cut
+
+sub getDistroPackages
+{
+    my ( $self ) = @_;
+
+    ();
+}
+
+=item buildConfFile( $file, [ $dest = $file [, \%mdata = { } [, \%pdata [, \%params = { } ] ] ] ] )
+
+ Build the given package configuration file
+ 
+ The following events are triggered:
+  - onLoadTemplate('<PNAME>', $filename, \$cfgTpl, $mdata, $pdata, $self->{'config'}, $params )
+  - before<PNAME>BuildConfFile( \$cfgTpl, $filename, \$dest, $mdata, $pdata, $self->{'config'}, $params )
+  - after<PNAME>BuildConfFile( \$cfgTpl, $filename, \$dest, $mdata, $pdata, $self->{'config'}, $params )
+
+  where <PNAME> is the package name as returned by the iMSCP::Packages::Abstract::getPackageName() method.
+
+ Param string|iMSCP::File $file An iMSCP::File object, an absolute filepath or a filepath relative to this package configuration directory
+ Param string $dest OPTIONAL Destination file path, default to $file
+ Param hashref \%mdata OPTIONAL Data as provided by the iMSCP::Modules::* modules, none if outside of an i-MSCP module context
+ Param hashref \%pdata OPTIONAL Package data (Package data have higher precedence than modules data)
+ Param hashref \%params OPTIONAL parameters:
+  - umask   : UMASK(2) for a new file. For instance if the given umask is 0027, mode will be: 0666 & ~0027 = 0640 (in octal)
+  - user    : File owner (default: EUID for a new file, no change for existent file)
+  - group   : File group (default: EGID for a new file, no change for existent file)
+  - mode    : File mode (default: 0666 & ~(UMASK(2) || 0) for a new file, no change for existent file )
+  - cached  : Whether or not loaded file must be cached in memory
+  - srcname : Make it possible to override default source filename passed into event listeners. Most used when file is a TMPFILE(3) file
+  - create  : Whether $dest must be created when $file doesn't exist and its content is not set (empty). An error is raised by default.
+ Return void, die on failure
+
+=cut
+
+sub buildConfFile
+{
+    my ( $self, $file, $dest, $mdata, $pdata, $params ) = @_;
+    $mdata //= {};
+    $pdata //= {};
+    $params //= {};
+
+    defined $file or croak( 'Missing or undefined $file parameter' );
+
+    # Force interpolation as $file can be a stringyfiable iMSCP::File object
+    $dest //= "$file";
+
+    my ( $pname, $cfgTpl ) = ( $self->getPackageName(), undef );
+    my ( $filename, $path ) = fileparse( $file );
+    $params->{'srcname'} //= $filename;
+
+    if ( $params->{'cached'} && exists $self->{'_templates'}->{"$file"} ) {
+        $file = $self->{'_templates'}->{"$file"};
+    } else {
+        # Trigger the onLoadTemplate event so that 3rd-party components are
+        # able to override default template
+        $self->{'eventManager'}->trigger( 'onLoadTemplate', lc $pname, $params->{'srcname'}, \$cfgTpl, $mdata, $pdata, $self->{'config'}, $params );
+
+        if ( length $cfgTpl ) {
+            # Template has been overridden by an event listener
+            if ( ref $file eq 'iMSCP::File' ) {
+                $file->set( $cfgTpl );
+            } else {
+                $file = iMSCP::File->new( filename => $file )->set( $cfgTpl );
+            }
+
+            undef $cfgTpl;
+        } elsif ( ref $file ne 'iMSCP::File' ) {
+            $file = iMSCP::File->new( filename => index( $path, '/' ) != 0 ? File::Spec->canonpath( "$self->{'cfgDir'}/$path/$filename" ) : $file );
+        }
+
+        $self->{'_templates'}->{"$file"} = $file if $params->{'cached'};
+    }
+
+    # Localizes the changes as we want keep the template clean (template caching)
+    local $file->{'file_content'} if $params->{'cached'};
+
+    # If $file doesn't exist and its content is not set (empty),
+    # raise an error, unless caller asked for $dest creation.
+    $cfgTpl = $file->getAsRef( !$params->{'create'} ? FALSE : !-f $file );
+
+    # Triggers the before<PNAME>BuildConfFile event so that 3rd-party
+    # components are able to act on the template
+    $self->{'eventManager'}->trigger(
+        "before${pname}BuildConfFile", $cfgTpl, $params->{'srcname'}, \$dest, $mdata, $pdata, $self->{'config'}, $params
+    );
+
+    # Expands the template variables using package and module data.
+    # Package data have higher priority.
+    processByRef( $pdata, $cfgTpl ) if %{ $pdata };
+    processByRef( $mdata, $cfgTpl ) if %{ $mdata };
+
+    # Triggers the after<PNAME>BuildConfFile event so that 3rd-party components
+    # are able to act on the template
+    $self->{'eventManager'}->trigger(
+        "after${pname}BuildConfFile", $cfgTpl, $params->{'srcname'}, \$dest, $mdata, $pdata, $self->{'config'}, $params
+    );
+
+    # Locally update the file path according to the desired destination if
+    # needed. We operate locally because the caller can have provided an
+    # iMSCP::File object, in which case it is not desirable to propagate the
+    # change.
+    local $file->{'filename'} = File::Spec->canonpath( $dest ) unless "$file" eq $dest;
+
+    $file->save( $params->{'umask'} );
+    $file->owner( $params->{'user'} // -1, $params->{'group'} // -1 ) if defined $params->{'user'} || defined $params->{'group'};
+    $file->mode( $params->{'mode'} ) if defined $params->{'mode'};
+}
+
+=item AUTOLOAD()
+
+ Implements autoloading for undefined methods
+
+ The default implementation will raise an error for any method that is not known
+ to be called by the iMSCP::Modules::Abstract class.
+
+ Return void, die on failure
+
+=cut
+
+sub AUTOLOAD
+{
+    ( my $method = our $AUTOLOAD ) =~ s/.*:://;
+
+    $method =~ /^
+        (?:pre|post)?
+        (?:add|disable|restore|delete)
+        (?:Domain|CustomDNS|FtpUser|Htaccess|Htgroup|Htpasswd|IpAddr|Mail|SSLcertificate|Subdomain|User)
+        $/x or die( sprintf( 'Unknown %s method', $AUTOLOAD ));
+
+    # Define the subroutine to prevent further AUTOLOADING
+    no strict 'refs';
+    *{ $AUTOLOAD } = sub {};
+
+    # Erase the stack frame
+    goto &{ $AUTOLOAD };
+}
+
+=item DESTROY( )
+
+ Destroy tasks
+
+=cut
+
+sub DESTROY
+{
+    # Needed due to AUTOLOAD
+}
+
+=back
+
+=head1 PRIVATE METHODS
+
+=over 4
+
+=item _init( )
+
+ See iMSCP::Common::Singleton::_init()
+
+=cut
+
+sub _init
+{
+    my ( $self ) = @_;
+
+    ref $self ne __PACKAGE__ or croak( sprintf( 'The %s class is an abstract class which cannot be instantiated', __PACKAGE__ ));
+
+    $self->{'eventManager'} = iMSCP::EventManager->getInstance();
+    $self->{'dbh'} = iMSCP::Database->getInstance();
+    $self->_loadConfig();
+    $self;
+}
+
+=item _loadConfig( [ $filename = lc( $self->getPackageName() . 'data ) ] )
+
+ Load the package configuration
+ 
+ In installer context, also merge the old configuration with new configuration and make
+ old configuration available through the 'old_config' attribute.
+
+ Param string $filename OPTIONAL i-MSCP package configuration filename
+ Return void, die on failure
+
+=cut
+
+sub _loadConfig
+{
+    my ( $self, $filename ) = @_;
+    $filename //= lc( $self->getPackageName() . '.data' );
+
+    defined $filename or croak( 'Missing $filename parameter' );
+    defined $self->{'cfgDir'} or croak( sprintf( "The %s class must define the 'cfgDir' property", ref $self ));
+
+    if ( iMSCP::Getopt->context() eq 'installer' && -f "$self->{'cfgDir'}/$filename.dist" ) {
+        if ( -f "$self->{'cfgDir'}/$filename" ) {
+            debug( sprintf( 'Merging old %s package configuration with new %s package configuration...', $filename, "$filename.dist" ));
+
+            tie my %oldConfig, 'iMSCP::Config',
+                filename => "$self->{'cfgDir'}/$filename",
+                readonly => TRUE,
+                # We do not want croak when accessing non-existing parameters
+                # in old configuration file. The new configuration file can
+                # refers to old parameters for new parameter values but in case
+                # the parameter doesn't exist in old conffile, we want simply
+                # an empty value. 
+                nocroak  => TRUE;
+
+            # Sometime, a configuration parameter get renamed. In such case the
+            # developer could want set the new parameter value with the old
+            # parameter name as a placeholder. For instance:
+            #
+            # Old parameter: DATABASE_USER
+            # New parameter: FTP_SQL_USER
+            #
+            # The value of the new parameter should be set as follows in the
+            # configuration file:
+            #
+            #   FTP_SQL_USER = {DATABASE_USER}
+            #
+            # By doing this, the value of the old DATABASE_USER parameter will
+            # be automatically used as value for the new FTP_SQL_USER parameter.
+            my $file = iMSCP::File->new( filename => "$self->{'cfgDir'}/$filename.dist" );
+            processByRef( \%oldConfig, $file->getAsRef(), TRUE );
+            $file->save();
+            undef( $file );
+
+            tie my %newConfig, 'iMSCP::Config', filename => "$self->{'cfgDir'}/$filename.dist";
+
+            while ( my ( $key, $value ) = each( %oldConfig ) ) {
+                $newConfig{$key} = $value if exists $newConfig{$key};
+            }
+
+            # Make the old configuration available through the 'old_config' property
+            %{ $self->{'old_config'} } = %oldConfig;
+
+            untie %newConfig;
+            untie %oldConfig;
+        } else {
+            # For a fresh installation, we make the configuration file free of any placeholder
+            my $file = iMSCP::File->new( filename => "$self->{'cfgDir'}/$filename.dist" );
+            processByRef( {}, $file->getAsRef(), TRUE );
+            $file->save();
+            undef( $file );
+        }
+
+        iMSCP::File->new( filename => "$self->{'cfgDir'}/$filename.dist" )->move( "$self->{'cfgDir'}/$filename" );
+    }
+
+    debug( sprintf( 'Loading %s package configuration...', $self->getPackageName()));
+
+    tie %{ $self->{'config'} },
+        'iMSCP::Config',
+        filename    => "$self->{'cfgDir'}/$filename",
+        readonly    => iMSCP::Getopt->context() ne 'installer',
+        nodeferring => iMSCP::Getopt->context() eq 'installer';
+
+    # Make the new configuration also available through the 'old_config'
+    # property, unless the property is alreadys set, or if we are not in installer context
+    %{ $self->{'old_config'} } = %{ $self->{'config'} } unless exists $self->{'old_config'} || iMSCP::Getopt->context() ne 'installer';
+}
+
+=item _installPackages( @packages )
+
+ Install distribution packages
+
+ Param list @packages List of packages to install
+ Return void, die on failure
+
+=cut
+
+sub _installPackages
+{
+    my ( undef, @packages ) = @_;
+
+    return unless @packages && !iMSCP::Getopt->skippackages;
+
+    iMSCP::DistPackageManager->getInstance()->installPackages( @packages );
+}
+
+=item _removePackages( @packages )
+
+ Remove distribution packages
+
+ Param list @packages Packages to remove
+ Return int 0 on success, other on failure
+
+=cut
+
+sub _removePackages
+{
+    my ( undef, @packages ) = @_;
+
+    return unless @packages && !iMSCP::Getopt->skippackages;
+
+    iMSCP::DistPackageManager->getInstance()->uninstallPackages( @packages );
+}
+
+=back
+
+=head1 AUTHOR
+
+ Laurent Declercq <l.declercq@nuxwin.com>
+
+=cut
+
+1;
+__END__
---
+++ backend/PerlLib/iMSCP/Packages/AbstractCollection.pm
@@ -0,0 +1,380 @@
+=head1 NAME
+
+ iMSCP::Packages::AbstractCollection - Abstract collection implementation for i-MSCP packages
+
+=cut
+
+# i-MSCP - internet Multi Server Control Panel
+# Copyright (C) 2010-2018 Laurent Declercq <l.declercq@nuxwin.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+package iMSCP::Packages::Collection;
+
+use strict;
+use warnings;
+use Carp qw/ croak /;
+use File::Basename qw/ dirname /;
+use iMSCP::Debug qw/ debug /;
+use iMSCP::Dir;
+use iMSCP::Getopt;
+use iMSCP::Dialog::InputValidation qw/ isOneOfStringsInList /;
+use Carp qw/ confess croak /;
+use parent 'iMSCP::Packages::Abstract';
+
+=head1 DESCRIPTION
+
+ This class provides an abstract collection implementation for the i-MSCP packages.
+
+=head1 PUBLIC METHODS
+
+=over 4
+
+=item registerSetupListeners( )
+
+ See iMSCP::Packages::Abstract::registerSetupListeners()
+
+=cut
+
+sub registerSetupListeners
+{
+    my ( $self ) = @_;
+
+    $self->{'eventManager'}->registerOne( 'beforeSetupDialog', sub { push @{ $_[0] }, sub { $self->showDialog( @_ ) }; } );
+}
+
+=item showDialog( \%dialog )
+
+ Show dialog
+
+ Param iMSCP::Dialog \%dialog
+ Return int 0 (NEXT), 30 (BACK) or 50 (ESC)
+
+=cut
+
+sub showDialog
+{
+    my ( $self, $dialog ) = @_;
+
+    my $packageName = $self->getPackageName();
+    my $ucPackageName = uc $packageName;
+
+    @{ $self->{'SELECTED_PACKAGES'} } = split(
+        ',', ::setupGetQuestion( "${ucPackageName}_PACKAGES", iMSCP::Getopt->preseed ? join( ',', @{ $self->{'AVAILABLE_PACKAGES'} } ) : '' )
+    );
+
+    my %choices;
+    @choices{@{ $self->{'AVAILABLE_PACKAGES'} }} = @{ $self->{'AVAILABLE_PACKAGES'} };
+
+    if ( isOneOfStringsInList( iMSCP::Getopt->reconfigure, [ lc $packageName, 'all', 'forced' ] )
+        || !@{ $self->{'SELECTED_PACKAGES'} } || grep { !exists $choices{$_} && $_ ne 'no' } @{ $self->{'SELECTED_PACKAGES'} }
+    ) {
+        ( my $rs, $self->{'SELECTED_PACKAGES'} ) = $dialog->checkbox(
+            <<"EOF", \%choices, [ grep { exists $choices{$_} && $_ ne 'no' } @{ $self->{'SELECTED_PACKAGES'} } ] );
+
+Please select the $packageName packages you want to install:
+\\Z \\Zn
+EOF
+        return $rs unless $rs < 30;
+    }
+
+    @{ $self->{'SELECTED_PACKAGES'} } = grep ( $_ ne 'no', @{ $self->{'SELECTED_PACKAGES'} } );
+
+    ::setupSetQuestion( "${ucPackageName}_PACKAGES", @{ $self->{'SELECTED_PACKAGES'} } ? join ',', @{ $self->{'SELECTED_PACKAGES'} } : 'no' );
+
+    for ( $self->getCollection() ) {
+        next unless $_->can( 'showDialog' );
+
+        debug( sprintf( 'Executing showDialog action on %s', $fpackage ));
+        my $rs = $_->showDialog();
+        return $rs if $rs;
+    }
+
+    0;
+}
+
+=item preinstall( )
+
+ See iMSCP::Packages::Abstract::preinstall()
+
+=cut
+
+sub preinstall
+{
+    my ( $self ) = @_;
+
+    my @distroPackages = ();
+    #    for my $package ( @{ $self->{'AVAILABLE_PACKAGES'} } ) {
+    #        next if grep ( $package eq $_, @{ $self->{'SELECTED_PACKAGES'} });
+    #        $package = "iMSCP::Packages::Webmail::${package}::${package}";
+    #        eval "require $package" or die( $@ );
+    #
+    #        if ( my $subref = $package->can( 'uninstall' ) ) {
+    #            debug( sprintf( 'Executing uninstall action on %s', $package ));
+    #            $subref->( $package->getInstance( eventManager => $self->{'eventManager'} ));
+    #        }
+    #
+    #        ( my $subref = $package->can( 'getDistroPackages' ) ) or next;
+    #        debug( sprintf( 'Executing getDistroPackages action on %s', $package ));
+    #        push @distroPackages, $subref->( $package->getInstance( eventManager => $self->{'eventManager'} ));
+    #    }
+    #
+    #    $self->_removePackages( @distroPackages );
+
+    @distroPackages = ();
+    for ( $self->getCollection() ) {
+        debug( sprintf( 'Executing preinstall action on %s', $fpackage ));
+        $_->preinstall();
+
+        debug( sprintf( 'Executing getDistroPackages action on %s', $fpackage ));
+        push @distroPackages, $_->getDistroPackages();
+    }
+
+    $self->_installPackages( @distroPackages );
+}
+
+=item install( )
+
+ See iMSCP::Packages::Abstract::install()
+
+=cut
+
+sub install
+{
+    for ( $self->getCollection() ) {
+        debug( sprintf( 'Executing install action on %s', $fpackage ));
+        $_->install();
+    }
+}
+
+=item postinstall( )
+
+ See iMSCP::Packages::Abstract::postinstall()
+
+=cut
+
+sub postinstall
+{
+    my ( $self ) = @_;
+
+    for ( $self->getCollection() ) {
+        debug( sprintf( 'Executing postinstall action on %s', $fpackage ));
+        $_->postinstall();
+    }
+}
+
+=item preuninstall( )
+
+ See iMSCP::Packages::Abstract::preuninstall()
+
+=cut
+
+sub preuninstall
+{
+    my ( $self ) = @_;
+}
+
+=item uninstall( )
+
+ See iMSCP::Packages::Abstract::uninstall()
+
+=cut
+
+sub uninstall
+{
+    my ( $self ) = @_;
+
+    my @distroPackages = ();
+    for ( $self->getCollection() ) {
+        debug( sprintf( 'Executing uninstall action on %s', ref $_ ));
+        $_->uninstall();
+
+        debug( sprintf( 'Executing getDistroPackages action on %s', $fpackage ));
+        push @distroPackages, $_->getDistroPackages();
+    }
+
+    $self->_removePackages( @distroPackages );
+}
+
+=item postuninstall( )
+
+ See iMSCP::Packages::Abstract::postuninstall()
+
+=cut
+
+sub postuninstall
+{
+    my ( $self ) = @_;
+
+    for ( $self->getCollection() ) {
+        debug( sprintf( 'Executing postuninstall action on %s', $fpackage ));
+        $_->postuninstall();
+    }
+}
+
+=item setBackendPermissions( )
+
+ See iMSCP::Packages::Abstract::setBackendPermissions()
+
+=cut
+
+sub setBackendPermissions
+{
+    my ( $self ) = @_;
+
+    for ( $self->getCollection() ) {
+        debug( sprintf( 'Executing setBackendPermissions action on %s', $fpackage ));
+        $_->setBackendPermissions();
+    }
+}
+
+=item setFrontendPermissions( )
+
+ See iMSCP::Packages::Abstract::setFrontendPermissions()
+
+=cut
+
+sub setFrontendPermissions
+{
+    my ( $self ) = @_;
+
+    for ( $self->getCollection() ) {
+        debug( sprintf( 'Executing setFrontendPermissions action on %s', $fpackage ));
+        $_->setFrontendPermissions();
+    }
+}
+
+=item dpkgPostInvokeTasks()
+
+ See iMSCP::Packages::Abstract::dpkgPostInvokeTasks()
+
+=cut
+
+sub dpkgPostInvokeTasks
+{
+    my ( $self ) = @_;
+
+    for ( $self->getCollection() ) {
+        debug( sprintf( 'Executing dpkgPostInvokeTasks action on %s', $fpackage ));
+        $_->dpkgPostInvokeTasks();
+    }
+}
+
+=item getCollection()
+
+ Get list of selected packages from this collection, sorted in descending order of priority
+
+ Return list of packages
+
+=cut
+
+sub getCollection
+{
+    my ( $self ) = @_;
+
+    CORE::state @collection = sort { $b->getPackagePriority() <=> $a->getPackagePriority() } map {
+        my $package = "iMSCP::Packages::@{ [ $self->getPackageName() ] }::$_::$_";
+        eval "require $package; 1" or die( $@ );
+        $package->getInstance();
+    } $self->{'SELECTED_PACKAGES'};
+
+    @collection;
+}
+
+=item AUTOLOAD()
+
+ Implements autoloading for undefined methods
+
+ The default implementation will raise an error for any method that is not known
+ to be called by the iMSCP::Modules::Abstract class.
+
+ Return void, die on failure
+
+=cut
+
+sub AUTOLOAD
+{
+    ( my $method = our $AUTOLOAD ) =~ s/.*:://;
+
+    $method =~ /^
+        (?:pre|post)?
+        (?:add|disable|restore|delete)
+        (?:Domain|CustomDNS|FtpUser|Htaccess|Htgroup|Htpasswd|IpAddr|Mail|SSLcertificate|Subdomain|User)
+        $/x or die( sprintf( 'Unknown %s method', $AUTOLOAD ));
+
+    # Define the method
+    no strict 'refs';
+    *{ $AUTOLOAD } = sub {
+        my ( $self ) = @_;
+
+        for ( $self->getCollection() ) {
+            debug( sprintf( 'Executing %s action on %s', $method, $fpackage ));
+            $_->$method();
+        }
+    };
+
+    # Erase the stack frame by calling the method
+    goto &{ $AUTOLOAD };
+}
+
+=back
+
+=head1 PRIVATE METHODS
+
+=over 4
+
+=item _init( )
+
+ See iMSCP::Packages::Abstract::_init()
+
+=cut
+
+sub _init
+{
+    my ( $self ) = @_;
+
+    ref $self ne __PACKAGE__ or croak( sprintf( 'The %s class is an abstract class which cannot be instantiated', __PACKAGE__ ));
+
+    if ( iMSCP::Getopt->context() eq 'installer' ) {
+        @{ $self->{'AVAILABLE_PACKAGES'} } = iMSCP::Dir->new( dirname => dirname( __FILE__ ) . '/' . $self->getPackageName())->getDirs();
+    }
+
+    @{ $self->{'SELECTED_PACKAGES'} } = grep $_ ne 'no', split( ',', $::imscpConfig{'WEBSTATS_PACKAGES'} );
+    $self->SUPER::_init();
+}
+
+=item _loadConfig( [ $filename = lc( $self->getPackageName() . 'data ) ] )
+
+ See iMSCP::Packages::Abstract::_loadConfig()
+
+=cut
+
+sub _loadConfig
+{
+    my ( $self, $filename ) = @_;
+
+    # Nothing to do here (collection)
+}
+
+=back
+
+=head1 AUTHOR
+
+ Laurent Declercq <l.declercq@nuxwin.com>
+
+=cut
+
+1;
+__END__
