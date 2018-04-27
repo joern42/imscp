ca0890979551774304d43502e0cccdb899f39a49
Laurent Declercq <l.declercq@nuxwin.com>
2018-04-27 04:56:07 +0200

    Added: iMSCP\Functions\Login::su() - Function making possible to become another identity during a login session Review: Login functions

--- frontend/library/classes/Functions/Login.php
+++ frontend/library/classes/Functions/Login.php
@@ -22,9 +22,16 @@ namespace iMSCP\Functions;
 
 use iMSCP\Application;
 use iMSCP\Authentication\AuthEvent;
+use iMSCP\Authentication\AuthResult;
 use iMSCP\Authentication\Handler\Credentials;
+use iMSCP\Model\SuIdentity;
+use iMSCP\Model\SuIdentityInterface;
+use iMSCP\Model\UserIdentity;
+use iMSCP\Model\UserIdentityInterface;
 use iMSCP\Plugin\Bruteforce;
-use Zend\EventManager\Event;
+use Zend\Db\Adapter\Driver\ResultInterface;
+use Zend\Db\ResultSet\HydratingResultSet;
+use Zend\Hydrator\Reflection as ReflectionHydrator;
 
 /**
  * Class Login
@@ -52,59 +59,73 @@ class Login
         $events->attach(AuthEvent::EVENT_AUTHENTICATION, [Credentials::class, 'authenticate'], 99);
 
         // Register listener that is responsible to check domain status and expire date
-        //$events->attach(Events::onBeforeSetIdentity, [Login::class, 'checkDomainAccountHandler']);
+        $events->attach(AuthEvent::EVENT_AFTER_AUTHENTICATION, [Login::class, 'checkDomainAccountListener']);
     }
 
     /**
-     * Check domain account state (status and expires date)
+     * Event listener that check domain account (status and expires date)
      *
-     * Note: Listen to the onBeforeSetIdentity event triggered in the Auth component.
+     * Listen on the AuthEvent::EVENT_AFTER_AUTHENTICATION
      *
-     * @param Event $event
+     * @param AuthEvent $event
      * @return void
      */
-    public static function checkDomainAccountHandler(Event $event): void
+    public static function checkDomainAccountListener(AuthEvent $event): void
     {
-        /** @var $identity \stdClass */
-        $identity = $event->getParam('identity');
+        $authResult = $event->getAuthenticationResult();
+        if (!$authResult->isValid()) {
+            return;
+        }
 
-        if ($identity->admin_type != 'user') {
+        $identity = $authResult->getIdentity();
+        if ($identity->getUserType() !== 'user') {
             return;
         }
 
         $stmt = execQuery(
-            'SELECT domain_expires, domain_status, admin_status FROM domain JOIN admin ON(domain_admin_id = admin_id) WHERE domain_admin_id = ?',
-            [$identity->admin_id]
+            '
+                SELECT t1.domain_expires, t1.domain_status, t2.admin_status
+                FROM domain AS t1
+                JOIN admin AS t2. ON(t2.admin_id = t2.domain_admin_id)
+                WHERE domain_admin_id = ?
+            ',
+            [$identity->getUserId()]
         );
 
-        $event->stopPropagation();
-
         if (!$stmt->rowCount()) {
-            writeLog(sprintf('Account data not found in database for the %s user', $identity->admin_name), E_USER_ERROR);
-            setPageMessage(tr('An unexpected error occurred. Please contact your reseller.'), 'error');
+            writeLog(sprintf('Account data not found in database for the %s user', $identity->getUsername()), E_USER_ERROR);
+            $authResult = new AuthResult(AuthResult::FAILURE_UNCATEGORIZED, $identity, [
+                tr('An unexpected error occurred. Please contact your reseller.')
+            ]);
+            $event->setAuthenticationResult($authResult);
             return;
         }
 
         $row = $stmt->fetch();
 
         if ($row['admin_status'] == 'disabled' || $row['domain_status'] == 'disabled') {
-            setPageMessage(tr('Your account has been disabled. Please, contact your reseller.'), 'error');
+            $authResult = new AuthResult(AuthResult::FAILURE_UNCATEGORIZED, $identity, [
+                tr('Your account has been disabled. Please, contact your reseller.')
+            ]);
+            $event->setAuthenticationResult($authResult);
             return;
         }
 
-        if ($row['domain_expires'] > 0 && $row['domain_expires'] < time()) {
-            setPageMessage(tr('Your account has expired. Please, contact your reseller.'), 'error');
-            return;
+        // We prevent login for accounts which are already expired or which will expire in less than one hour
+        if ($row['domain_expires'] > 0 && ($row['domain_expires'] - 3599) < time()) {
+            setPageMessage(tr('Your account is expired or will expire in less than one hour. Please contact your reseller.'), 'error');
+            $authResult = new AuthResult(AuthResult::FAILURE_UNCATEGORIZED, $identity, [
+                tr('Your account has expired. Please, contact your reseller.')
+            ]);
+            $event->setAuthenticationResult($authResult);
         }
-
-        $event->stopPropagation(false);
     }
 
     /**
      * Remove data that belong to expired sessions
      *
      * FIXME: Shouldn't be run on every requests.
-     * 
+     *
      * @return void
      */
     public static function doSessionTimeout(): void
@@ -112,7 +133,7 @@ class Login
         // We must not remove bruteforce plugin data (AND `user_name` IS NOT NULL)
         execQuery(
             'DELETE FROM login WHERE lastaccess < ? AND user_name IS NOT NULL', [
-                time() - Application::getInstance()->getConfig()['SESSION_TIMEOUT'] * 60
+            time() - Application::getInstance()->getConfig()['SESSION_TIMEOUT'] * 60
         ]);
     }
 
@@ -135,18 +156,18 @@ class Login
         }
 
         $identity = $authService->getIdentity();
-        $session = Application::getInstance()->getSession();
 
         // When the panel is in maintenance mode, only administrators can access the interface
-        if (Application::getInstance()->getConfig()['MAINTENANCEMODE'] && $identity->admin_type != 'admin'
-            && (!isset($session['logged_from_type']) || $session['logged_from_type'] != 'admin')
+        if (Application::getInstance()->getConfig()['MAINTENANCEMODE'] && $identity->getUserType() != 'admin'
+            && ((!($identity instanceof SuIdentityInterface)
+                || ($identity->getSuUserType() != 'admin' && !($identity->getSuIdentity() instanceof SuIdentityInterface))))
         ) {
             $authService->clearIdentity();
             redirectTo('/index.php');
         }
 
         // Check user level
-        if (empty($userLevel) || ($userLevel !== 'all' && $identity->admin_type != $userLevel)) {
+        if (empty($userLevel) || ($userLevel !== 'all' && $identity->getUserType() != $userLevel)) {
             $authService->clearIdentity();
             redirectTo('/index.php');
         }
@@ -159,92 +180,107 @@ class Login
             View::showForbiddenErrorPage();
         }
 
-        // If all goes fine update session and last access
+        // If all condition are meet, update session and last access
+        $session = Application::getInstance()->getSession();
         $session['user_login_time'] = time();
         execQuery('UPDATE login SET lastaccess = ? WHERE session_id = ?', [$session['user_login_time'], session_id()]);
     }
 
     /**
-     * Switch between user's interfaces
+     * Become another user or switch back to previous user
      *
-     * @param int $fromId User ID to switch from
-     * @param int $toId User ID to switch on
+     * @param int|null $userId Unique identifier of user to become, NULL to switch back to previous user
      * @return void
      */
-    public static function changeUserInterface(int $fromId, int $toId): void
+    public static function su(int $userId = NULL): void
     {
-        $toActionScript = false;
-
-        while (1) { // We loop over nothing here, it's just a way to avoid code repetition
-            $stmt = execQuery(
-                'SELECT admin_id, admin_name, admin_type, email, created_by FROM admin WHERE admin_id IN(?, ?) ORDER BY FIELD(admin_id, ?, ?) LIMIT 2',
-                [$fromId, $toId, $fromId, $toId]
-            );
-
-            if ($stmt->rowCount() < 2) {
-                setPageMessage(tr('Bad request.'), 'error');
-            }
-
-            list($from, $to) = $stmt->fetchAll(\PDO::FETCH_OBJ);
-
-            $fromToMap = [];
-            $fromToMap['admin']['BACK'] = 'users.php';
-            $fromToMap['admin']['reseller'] = 'index.php';
-            $fromToMap['admin']['user'] = 'index.php';
-            $fromToMap['reseller']['user'] = 'index.php';
-            $fromToMap['reseller']['BACK'] = 'users.php';
-
-            $session = Application::getInstance()->getSession();
-
-            if (!isset($fromToMap[$from->admin_type][$to->admin_type]) || ($from->admin_type == $to->admin_type)) {
-                if (!isset($session['logged_from_id']) || $session['logged_from_id'] != $to->admin_id) {
-                    setPageMessage(tr('Bad request.'), 'error');
-                    writeLog(sprintf("%s tried to switch onto %s's interface", $from->admin_name, decodeIdna($to->admin_name)), E_USER_WARNING);
-                    break;
-                }
-
-                $toActionScript = $fromToMap[$to->admin_type]['BACK'];
-            }
-
-            $toActionScript = $toActionScript ?: $fromToMap[$from->admin_type][$to->admin_type];
-
-            // Set new identity
-            $authService = Application::getInstance()->getAuthService();
-            $authService->clearIdentity();
-
-            if ($from->admin_type != 'user' && $to->admin_type != 'admin') {
-                $session = Application::getInstance()->getSession();
-                // Set additional data about user from which we are logged from
-                $session['logged_from_type'] = $from->admin_type;
-                $session['logged_from'] = $from->admin_name;
-                $session['logged_from_id'] = $from->admin_id;
-                writeLog(sprintf("%s switched onto %s's interface", $from->admin_name, decodeIdna($to->admin_name)), E_USER_NOTICE);
-            } else {
-                writeLog(sprintf("%s switched back from %s's interface", $to->admin_name, decodeIdna($from->admin_name)), E_USER_NOTICE);
-            }
-
-            $authService->setIdentity($to);
-            break;
+        $authService = Application::getInstance()->getAuthService();
+        if (!$authService->hasIdentity()) {
+            // Guest users cannot become another user
+            View::showBadRequestErrorPage();
         }
 
-        static::redirectToUiLevel($toActionScript);
+        $identity = $authService->getIdentity();
+
+        if (NULL != $userId && $identity->getUserId() === $userId) {
+            // An user cannot become himself
+            View::showBadRequestErrorPage();
+        }
+
+        $newIdentity = NULL;
+
+        if ($identity instanceof SuIdentityInterface) { // Administrator or Reseller logged-in as another user
+            if ($identity->getSuIdentity() instanceof SuIdentityInterface) { // Administrator logged-in as 'reseller', then logged-in as 'user'
+                if (NULL !== $userId) {
+                    // When logged-in as 'reseller', then logged-in as 'user', an administrator
+                    // can only become the previous user, that is, a 'reseller'.
+                    View::showBadRequestErrorPage();
+                }
+
+                # and that wants become back the previous user
+                $newIdentity = $identity->getSuIdentity();
+            } else { // Administrator logged-in as 'reseller' or 'user', Or Reseller logged-in as 'user'
+                if ($identity->getSuUserType() === 'admin') {
+                    if ($identity->getUserType() === 'reseller') { // Administrator logged-in as 'reseller'
+                        if (NULL !== $userId) { // and that want become a 'user' of that 'reseller'
+                            $newIdentity = new SuIdentity($identity, static::getIdentityFromDb($userId));
+                        } else { // and that wants become himself
+                            $newIdentity = $identity->getSuIdentity();
+                        }
+                    } elseif ($identity->getUserType() === 'user') { // Administrator logged-in as 'user'
+                        if (NULL !== $userId) {
+                            // Unsupported use case: An administrator logged-in as 'user' can only become himself
+                            View::showBadRequestErrorPage();
+                        }
+
+                        // and that wants become back to himself
+                        $newIdentity = $identity->getSuIdentity();
+                    } else {
+                        // Unsupported use case: Unknown user identity type
+                        View::showBadRequestErrorPage();
+                    }
+                } elseif ($identity->getSuUserType() == 'reseller') { // Reseller logged-in as user
+                    if (NULL !== $userId) {
+                        // Unsupported use case: A reseller logged-in as user can only become himself
+                        View::showBadRequestErrorPage();
+                    }
+
+                    // and that wants become back to himself
+                    $newIdentity = $identity->getSuIdentity();
+                } else {
+                    // Unsupported use case: Unknown SU identity type; A SU identity can be only of type 'admin' or 'reseller'
+                    View::showBadRequestErrorPage();
+                }
+            }
+        } elseif (NULL === $userId || $identity->getUserType() == 'user') {
+            // Unsupported use case: A non SU identity cannot become back to himself and an 'user' identity cannot become another user
+            View::showBadRequestErrorPage();
+        } else { // Administrator or Reseller that wants become another user
+            $newIdentity = new SuIdentity($identity, static::getIdentityFromDb($userId));
+
+            if ($identity->getUserType() === 'reseller' && $newIdentity->getUserCreatedBy() !== $identity->getUserId()) {
+                // A reseller cannot become an user that have not been created by himself
+                View::showBadRequestErrorPage();
+            }
+        }
+
+        $authService->setIdentity($newIdentity);
+        static::redirectToUiLevel();
     }
 
     /**
-     * Redirects to user ui level
+     * Redirects to user UI level
      *
-     * @param string $actionScript Action script on which user should be redirected
      * @return void
      */
-    public static function redirectToUiLevel(string $actionScript = 'index.php'): void
+    public static function redirectToUiLevel(): void
     {
         $authService = Application::getInstance()->getAuthService();
-
         if (!$authService->hasIdentity()) {
             return;
         }
 
-        switch ($authService->getIdentity()->admin_type) {
+        switch ($authService->getIdentity()->getUserType()) {
             case 'user':
                 $userType = 'client';
                 break;
@@ -258,6 +294,38 @@ class Login
                 throw new \Exception('Unknown UI level');
         }
 
-        redirectTo('/' . $userType . '/' . $actionScript);
+        redirectTo('/' . $userType . '/index.php');
+    }
+
+    /**
+     * Get an identity from the database
+     *
+     * @param int $identityId Identity unique identifier
+     * @return UserIdentityInterface
+     */
+    protected static function getIdentityFromDb(int $identityId): UserIdentityInterface
+    {
+        $stmt = Application::getInstance()->getDb()->createStatement(
+            'SELECT admin_id, admin_name, admin_pass, admin_type, email, created_by FROM admin WHERE admin_id = ?'
+        );
+        $stmt->prepare();
+        $result = $stmt->execute([$identityId]);
+
+        if ($result instanceof ResultInterface && $result->isQueryResult()) {
+            $resultSet = new HydratingResultSet(new ReflectionHydrator, new UserIdentity());
+            $resultSet->initialize($result);
+
+            if (count($resultSet) < 1) {
+                View::showBadRequestErrorPage();
+            }
+
+            /** @var UserIdentityInterface $identity */
+            $identity = $resultSet->current();
+            return $identity;
+        }
+
+        // Something else went wrong...
+        View::showInternalServerError();
+        exit;
     }
 }
