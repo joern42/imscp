37ab9e85fc93ba59a204766903998f4ab5bef335
Laurent Declercq <l.declercq@nuxwin.com>
2018-08-25 06:32:42 +0200

    Typo

--- engine/PerlLib/iMSCP/AbstractUninstallerActions.pm
+++ engine/PerlLib/iMSCP/AbstractUninstallerActions.pm
@@ -80,8 +80,8 @@ sub postuninstall
  
  In uninstaller context, processing of delayed tasks on the distribution
  package manager is triggered by the uninstaller after the call of the
- postinstall action on the packages and servers. Thus, those last SHOULD
- call this method in the postinstall action.
+ postuninstall action on the packages and servers. Thus, those last SHOULD
+ call this method in the postuninstall action.
 
  Param arrayref \@packages Array containing a list of distribution packages to uninstall
  Return int 0 on success, other or die on failure
