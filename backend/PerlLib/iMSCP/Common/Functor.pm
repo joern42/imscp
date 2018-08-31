4046da8d822ee44acbc70fb473e6436a69b78cb0
Laurent Declercq <l.declercq@nuxwin.com>
2018-08-31 10:36:59 +0200

    Small fix

--- backend/PerlLib/iMSCP/Common/Functor.pm
+++ backend/PerlLib/iMSCP/Common/Functor.pm
@@ -9,7 +9,7 @@ package iMSCP::Common::Functor;
 use strict;
 use warnings;
 use iMSCP::Boolean;
-use parent iMSCP::Common::Object;
+use parent 'iMSCP::Common::Object';
 use overload
     '&{}'    => sub { $_[0]->can( '__invoke' ) or die( sprintf( 'The %s class must implement the __invoke() method', ref $_[0] )); },
     fallback => TRUE;
