0f794cf8991463552fd0622dd846bc1922d4e4c5
Laurent Declercq <l.declercq@nuxwin.com>
2018-08-31 10:36:04 +0200

    Fixed: Wrong package name +++

--- backend/PerlLib/iMSCP/Common/Functor.pm
+++ backend/PerlLib/iMSCP/Common/Functor.pm
@@ -4,14 +4,15 @@
 
 =cut
 
-package Common::Functor;
+package iMSCP::Common::Functor;
 
 use strict;
 use warnings;
+use iMSCP::Boolean;
 use parent iMSCP::Common::Object;
-use overload '&{}' => sub {
-    $_[0]->can( '__invoke' ) or die( sprintf( 'The %s class must implement the __invoke() magic method', ref $_[0] ));
-};
+use overload
+    '&{}'    => sub { $_[0]->can( '__invoke' ) or die( sprintf( 'The %s class must implement the __invoke() method', ref $_[0] )); },
+    fallback => TRUE;
 
 =head1 DESCRIPTION
 
