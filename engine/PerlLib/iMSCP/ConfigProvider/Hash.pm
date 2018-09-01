4f1b87b79acd04c32d920aac0ca0f19108e21974
Laurent Declercq <l.declercq@nuxwin.com>
2018-09-01 07:40:55 +0200

    Added: Configuration provider for Perl hashes

---
+++ engine/PerlLib/iMSCP/ConfigProvider/Hash.pm
@@ -0,0 +1,71 @@
+=head1 NAME
+
+ iMSCP::ConfigProvider::Hash - Configuration provider for Perl hashes
+
+=cut
+
+package iMSCP::ConfigProvider::Hash;
+
+use strict;
+use warnings;
+use parent 'Common::Functor';
+
+=head1 DESCRIPTION
+
+ Provider that returns the hashref seeded to itself.
+
+ Primary use case is configuration cache-related settings.
+
+=head1 CLASS METHODS
+
+=over 4
+
+=item new( $hash)
+
+ Constructor
+ 
+ Param $hashref $hash A hash reference
+ Return iMSCP::ConfigProvider::Hash
+
+=cut
+
+sub new
+{
+    my ( $self, $hash ) = @_;
+
+    ref $hash eq 'HASH' or die( 'Invalid $hash parameter. HASH reference expected.' );
+
+    $self->SUPER::new( config => $hash );
+}
+
+=back
+
+=head1 PRIVATE METHODS
+
+=over 4
+
+=item __invoke()
+
+ Functor implementation
+
+ Return hashref on sucess, die on failure
+
+=cut
+
+sub __invoke
+{
+    my ( $self ) = @_;
+
+    $self->{'config'};
+}
+
+=back
+
+=head1 AUTHOR
+
+ Laurent Declercq <l.declercq@nuxwin.com>
+
+=cut
+
+1;
+__END__
