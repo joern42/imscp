765c4c9efeb5bba0e42a0545e79fc266b7c3df76
Laurent Declercq <l.declercq@nuxwin.com>
2018-09-29 12:08:55 +0200

    Review: Dialog frontEnds (Installer)

--- src/backend/lib/iMSCP/Dialog/FrontEndInterface.pm
+++ src/backend/lib/iMSCP/Dialog/FrontEndInterface.pm
@@ -5,21 +5,21 @@
 =cut
 
 # i-MSCP - internet Multi Server Control Panel
-# Copyright (C) 2010-2018 by Laurent Declercq <l.declercq@nuxwin.com>
+# Copyright (C) 2010-2018 Laurent Declercq <l.declercq@nuxwin.com>
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 package iMSCP::Dialog::FrontEndInterface;
 
@@ -87,7 +87,7 @@ use parent 'iMSCP::Common::SingletonClass';
 
 =cut
 
-=item inputbox( $text [, $default = '' ] )
+=item string( $text [, $default = '' ] )
 
  Show string box
 
@@ -103,7 +103,7 @@ use parent 'iMSCP::Common::SingletonClass';
 
  Param string $text Text to show
  Param string $default Default value
- Return string|list Password string or a list containing both DIALOG(1) exit code and password string
+ Return string|list Input password or a list containing both DIALOG(1) exit code and input password
 
 =cut
 
